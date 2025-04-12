6fd326ca17819df3b39759dbcadedb81618e5768
aik366 <haykooo3@gmail.com>
2025-04-12 15:12:52 +0300

    start 2

---
+++ bot.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer("Бот запущен в Docker!")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
